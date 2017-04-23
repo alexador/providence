8126e7a9bd96e83ae901dd028e6dc1207dc4b6e4
Alexander Dorofeev <karmiceft@gmail.com>
2017-04-23 23:51:39 +0300

    the plan chooser is done

--- markup/components/main-nav/main-nav.js
+++ markup/components/main-nav/main-nav.js
@@ -9,31 +9,12 @@ $(document).ready(function () {
     // Activate menu
     if (wScroll > 20) {
       $('.main-nav').addClass('main-nav_is-active');
-      // $('#slide_out_menu').addClass('scrolled');
     } else {
       $('.main-nav').removeClass('main-nav_is-active');
-      // $('#slide_out_menu').removeClass('scrolled');
     }
-
-
-    // Scroll Effects
-
   });
 
-
-  // Navigation
-  // $('#navigation').on('click', function (e) {
-  //     e.preventDefault();
-  //     $(this).addClass('open');
-  //     $('#slide_out_menu').toggleClass('open');
-
-  //     if ($('#slide_out_menu').hasClass('open')) {
-  //         $('.menu-close').on('click', function (e) {
-  //             e.preventDefault();
-  //             $('#slide_out_menu').removeClass('open');
-  //         });
-  //     }
-  // });
+  // slide out menu
   $('.main-nav__ham').on('click', function (e) {
     e.preventDefault();
     $('.main-nav__slide-menu').toggleClass('main-nav__slide-menu_open');
@@ -47,29 +28,24 @@ $(document).ready(function () {
 
 
   // Price Table
-  const individualPrices = $('.plan__services-wrapper').find('.plan__individual');
-  const companyPrices = $('.plan__services-wrapper').find('.plan__company');
+  const $plansForIndividual = $('.plan__individual');
+  const $plansForCompany = $('.plan__company');
 
-
-  $('.plan__toggle-wrapper').find('.plan__btn-individual').addClass('plan__btn-individual_acitve');
-  $('.plan__services-wrapper').find('.plan__individual').addClass('plan__individual_active');
-
-  $('.plan__toggle-wrapper').find('.plan__btn-individual').on('click', function () {
+  $('.plan__btn-individual').on('click', function () {
     $(this).addClass('plan__btn-individual_active');
     $(this).closest('.plan__toggle-wrapper').removeClass('plan__toggle-wrapper_switched');
     $(this).siblings().removeClass('plan__btn-company_active');
-    individualPrices.addClass('plan__individual_active');
-    companyPrices.removeClass('plan__company_active');
-  });
+    $plansForIndividual.addClass('plan__individual_active');
+    $plansForCompany.removeClass('plan__company_active');
+  })
 
-  $('.plan__toggle-wrapper').find('.plan__btn-company').on('click', function () {
+  $('.plan__btn-company').on('click', function () {
     $(this).addClass('plan__btn-company_active');
     $(this).closest('.plan__toggle-wrapper').addClass('plan__toggle-wrapper_switched');
-    $(this).siblings().removeClass('.plan__btn-individual_active');
-    companyPrices.addClass('plan__company_active');
-    individualPrices.removeClass('plan__individual_active');
-  });
-
+    $(this).siblings().removeClass('plan__btn-individual_active');
+    $plansForCompany.addClass('plan__company_active');
+    $plansForIndividual.removeClass('plan__individual_active');
+  })
 
   // Wow Animations
   // let wow = new WOW({
@@ -82,23 +58,7 @@ $(document).ready(function () {
   // wow.init();
 
 
-  // Menu For Xs Mobile Screens
-  // if ($(window).height() < 450) {
-  //     $('.main-nav__slide-footer').addClass('.main-nav__slide-footer_xs');
-  // }
-
-  // $(window).on('resize', function () {
-  //     if ($(window).height() < 450) {
-  //         $('.main-nav__slide-footer').addClass('.main-nav__slide-footer_xs');
-  //     } else {
-  //         $('.main-nav__slide-footer').removeClass('.main-nav__slide-footer_xs');
-  //     }
-  // });
-
-
   // Magnific Popup
   // $('.lightbox').magnificPopup();
-
-
 
 });
