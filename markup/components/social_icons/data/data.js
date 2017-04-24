6e80c2bd1cdae857416e66ad55d519f1d2ef88b7
Alexander Dorofeev <karmiceft@gmail.com>
2017-04-24 15:59:47 +0300

    raw version is done

--- markup/components/social_icons/data/data.js
+++ markup/components/social_icons/data/data.js
@@ -1,8 +1,10 @@
 social_icons: {
     default: {
+        wrap: 'social-icons',
         item: 'social-icons__item'
     },
     narrow: {
+        wrap: 'social-icons social-icons_narrow',
         item: 'social-icons__item social-icons__item_narrow'
     }
 }
